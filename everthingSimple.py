6b874850215b24bbf142d90e925b2209e8249a3f
Fatih Hakan Mehmet ÇAYLIDEMIRCI <cyldmrcfhm@gmail.com>
2022-01-03 18:27:59 +0300

    everthingSimple.py eklendi

---
+++ everthingSimple.py
@@ -0,0 +1,21 @@
+list = [-5, -10, -50, -2, 5, -6]
+
+isPositive = False
+
+for i in list:
+    if i > 0:
+        isPositive = True
+        break
+
+# print(isPositive)
+
+# any() liste de herhangi biri true ise True. Boşsa ve tümü yanlışsa False döndürür
+# all() tüm öğeler True ise veya liste boşsa True döndürür.
+
+list = [-5, -10, -50, -2, 5, -6]
+isPositive = any(i > 0 for i in list)
+# print(isPositive)
+
+
+isPositive = not all(i <= 0 for i in list)
+print(isPositive)
